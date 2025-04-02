1cc31cec1aa1b48b761a64ac1469b036afdd8981
vinay <vinkjaiswal@gmail.com>
2025-04-02 15:28:46 +0530

    Add custom grid based on user input Add button to change grid, prompt user, delete existing grid, create new one

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const container = document.querySelector('#container');
+const changeGridBtn = document.querySelector('button');
 
+createGrid(16);
+
+// change grid based on user input
+changeGridBtn.addEventListener('click', () => {
+  const noOfBoxes = prompt('How many boxes in a row? (max. 100):');
+  deleteGrid();
+  createGrid(noOfBoxes);
+});
+
+// delete grid
+function deleteGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
 // create 16x16 grid of square boxes
-for (let i = 1; i <= 16 * 16; i++) {
-  const box = document.createElement('div');
-  box.id = `box${i}`;
-  box.classList.add('box');
-  container.appendChild(box);
+function createGrid(rowSize) {
+  for (let i = 1; i <= rowSize * rowSize; i++) {
+    const box = document.createElement('div');
+    box.id = `box${i}`;
+    box.style.width = `${960 / rowSize}px`;
+    box.style.height = `${960 / rowSize}px`;
+    box.classList.add('box');
+    container.appendChild(box);
 
-  // add hover effect on the boxes
-  box.addEventListener('mouseover', () => {
-    box.style.backgroundColor = 'pink';
-  });
+    // add hover effect on the boxes
+    box.addEventListener('mouseover', () => {
+      box.style.backgroundColor = 'pink';
+    });
+  }
 }
