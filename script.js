52bbc2e2d2a547ed59a9099eeeee5607cf6a5376
vinay <vinkjaiswal@gmail.com>
2025-04-02 14:20:05 +0530

    Add hover effect on square boxes Add mouseover event listner on all square boxes in script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('#container');
 
-// create 16 square boxes
+// create 16x16 grid of square boxes
 for (let i = 1; i <= 16 * 16; i++) {
   const box = document.createElement('div');
   box.id = `box${i}`;
   box.classList.add('box');
   container.appendChild(box);
-}
 
-// rearrange boxes into grid using css
+  // add hover effect on the boxes
+  box.addEventListener('mouseover', () => {
+    box.style.backgroundColor = 'pink';
+  });
+}
